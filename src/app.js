911032b6e81b90bd78ab422590402f01935cd7d1
Yulia Khisamutdinova <yulia-kh@users.noreply.github.com>
2019-11-25 12:40:49 -0800

    add login endpoint

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const authRouter = require('./auth/auth-router');
 
 const app = express();
 
@@ -16,6 +17,8 @@ app.use(cors());
 app.get('/', (req, res) => {
   res.send('Hallo, Textbaustein!');
 });
+
+app.use('/api/auth', authRouter);
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
