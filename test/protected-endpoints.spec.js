7ebd64ae831e03312a2a7f948b962ba597be44a6
Hubert Yang <offspring7890@yahoo.com>
2019-11-27 09:55:28 -0800

    auth-endpoint tests passing

--- test/protected-endpoints.spec.js
+++ test/protected-endpoints.spec.js
@@ -2,7 +2,7 @@ const knex = require('knex')
 const app = require('../src/app')
 const helpers = require('./test-helpers')
 
-describe('Protected endpoints', function() {
+describe.skip('Protected endpoints', function() {
   let db
 
   const {
@@ -14,7 +14,7 @@ describe('Protected endpoints', function() {
   before('make knex instance', () => {
     db = knex({
       client: 'pg',
-      connection: process.env.TEST_DB_URL,
+      connection: process.env.TEST_DATABASE_URL,
     })
     app.set('db', db)
   })
