c262294ec6fa4e4c2a264dbbdb1cc01105340897
Hubert Yang <offspring7890@yahoo.com>
2019-11-27 08:11:27 -0800

    env file

--- src/config.js
+++ src/config.js
@@ -2,9 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  DATABASE_URL:
-    process.env.DATABASE_URL ||
-    'postgresql://dunder_mifflin@localhost/chorerunner',
+  DATABASE_URL: 'postgresql://dunder_mifflin@localhost/chorerunner',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 };
