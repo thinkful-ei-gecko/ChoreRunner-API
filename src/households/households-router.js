9313d69b64e237476defb6305eae9772ca6f2b9c
psychicbologna <alexander.fukui@gmail.com>
2019-12-02 14:54:39 -0600

    init branch, household endpoints

--- src/households/households-router.js
+++ src/households/households-router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const { requireAuth } = require('../middleware/jwt-auth')
 const HouseholdsService = require('./households-service');
-const {requireMemberAuth} = require('../middleware/member-jwt')
+const { requireMemberAuth } = require('../middleware/member-jwt')
 // const shortid = require('shortid');
 
 const householdsRouter = express.Router();
@@ -15,20 +15,20 @@ householdsRouter
     const { name } = req.body;
     const user_id = req.user.id;
 
-  console.log(name, user_id)
- 
+    console.log(name, user_id)
+
     if (!name)
-       return res.status(400).json({
+      return res.status(400).json({
         error: `Missing name in request body`,
       });
 
-  try {
-    //use short id to generate house code
-    // let house_code = `${name}` + shortid.generate();
-    const newHousehold = {
-      name,
-      user_id,
-    };
+    try {
+      //use short id to generate house code
+      // let house_code = `${name}` + shortid.generate();
+      const newHousehold = {
+        name,
+        user_id,
+      };
 
       const house = await HouseholdsService.insertHousehold(
         req.app.get('db'),
@@ -67,18 +67,19 @@ householdsRouter
     const { householdId } = req.params;
 
     if (!title || !member_id || !points) {
-      return res.status(400).json({error: {message: 'Missing task name, member id or points in request body'}});
+      return res.status(400).json({ error: { message: 'Missing task name, member id or points in request body' } });
     }
-    
+
     const newTask = {
-      title, 
+      title,
       household_id: householdId,
       // user_id, 
-      member_id, 
-      points};
+      member_id,
+      points
+    };
 
     newTask.user_id = req.user.id;
-    
+
     HouseholdsService.insertTask(
       req.app.get('db'),
       newTask
@@ -100,12 +101,12 @@ householdsRouter
         const result = {};
         tasks.forEach(task => {
           if (task.member_id in result) {
-            result[task.member_id].tasks.push({'title': task.title, 'id': task.id, 'points': task.points});
+            result[task.member_id].tasks.push({ 'title': task.title, 'id': task.id, 'points': task.points });
           } else {
             result[task.member_id] = {
-                              name: task.name,
-                              tasks: [{'title': task.title, 'id': task.id, 'points': task.points}]
-                            }
+              name: task.name,
+              tasks: [{ 'title': task.title, 'id': task.id, 'points': task.points }]
+            }
           }
         })
         return res.json(result)
@@ -113,43 +114,43 @@ householdsRouter
       .catch(next);
   })
 
-  //NOTE: THIS ENDPOINT USES THE MEMBER'S AUTHTOKEN, NOT PARAMS. 
-  //MIGHT WANT TO FIX THIS BEFORE DEPLOY
-  householdsRouter
-    .route('/householdId/members/memberId/tasks')
-    .all(requireMemberAuth)
-    .get((req, res, next) => {
-      console.log(req.member)
-      HouseholdsService.getMemberTasks(
-        req.app.get('db'),
-        req.member.household_id,
-        req.member.id,
-      )
+//NOTE: THIS ENDPOINT USES THE MEMBER'S AUTHTOKEN, NOT PARAMS. 
+//MIGHT WANT TO FIX THIS BEFORE DEPLOY
+householdsRouter
+  .route('/householdId/members/memberId/tasks')
+  .all(requireMemberAuth)
+  .get((req, res, next) => {
+    console.log(req.member)
+    HouseholdsService.getMemberTasks(
+      req.app.get('db'),
+      req.member.household_id,
+      req.member.id,
+    )
       .then(result => {
         res.status(201).json(result)
       })
       .catch(next)
+  })
+  .delete(jsonBodyParser, (req, res, next) => {
+    const { taskId } = req.body
+    console.log(taskId)
+    HouseholdsService.completeTask(
+      req.app.get('db'),
+      req.member.id,
+      req.member.household_id,
+      taskId
+    ).then(() => {
+      res.status(204).end()
     })
-    .delete(jsonBodyParser, (req, res, next) => {
-      const {taskId} =req.body
-      console.log(taskId)
-      HouseholdsService.completeTask(
-        req.app.get('db'),
-        req.member.id,
-        req.member.household_id,
-        taskId
-      ).then(() => {
-        res.status(204).end()
-      })
       .catch(next)
-    })
+  })
 
-  householdsRouter
+householdsRouter
   .route('/:householdId/members')
   .all(requireAuth)
   .get((req, res, next) => {
     const { householdId } = req.params;
-  
+
     return HouseholdsService.getAllMembers(
       req.app.get('db'),
       householdId
@@ -185,7 +186,7 @@ householdsRouter
       if (hasMemberwithMemberName)
         return res.status(400).json({ error: `Username already taken` })
 
-      const hashedPassword = await  HouseholdsService.hashPassword(password)
+      const hashedPassword = await HouseholdsService.hashPassword(password)
 
       const newMember = {
         username,
@@ -204,13 +205,13 @@ householdsRouter
         .status(201)
         .location(path.posix.join(req.originalUrl, `/${member.id}`))
         .json(HouseholdsService.serializeMember(member))
-    } catch(error) {
+    } catch (error) {
       next(error)
     }
   })
 
-  //delete household? 
+//delete household? 
 
-  //Update household? 
+//Update household? 
 
 module.exports = householdsRouter;
