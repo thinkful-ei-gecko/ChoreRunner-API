e8d12a402575973ccf3ecdf0961eef1f41854998
Hubert Yang <offspring7890@yahoo.com>
2019-12-05 11:30:03 -0800

    users-endpoint tests are passing except xss

--- test/households-endpoints.spec.js
+++ test/households-endpoints.spec.js
@@ -2,7 +2,7 @@ const knex = require('knex')
 const app = require('../src/app')
 const helpers = require('./test-helpers')
 
-describe.only('Households Endpoints', function () {
+describe.skip('Households Endpoints', function () {
   let db
 
   const {
